50c4981964f37e81507bf4e9334eec748795e9ed
pbhuniar <82756479+pbhuniar@users.noreply.github.com>
2021-06-02 15:32:44 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,15 +12,7 @@ function Menu() {
       <Container>
       <br/>
       <h4 style={{color:"white"}}>Time is 20 second from starting</h4>
-      <div className="text-box">
-//       <Form >
-//         <Form.Control  type="text"
-//         placeholder="Enter your name"
-//         onChange={(event) => {
-//           setUserName(event.target.value);
-//         }}/>
-//       </Form>
-      </div>
+ 
   
      <br/>
       <Button variant="success" size="lg"
